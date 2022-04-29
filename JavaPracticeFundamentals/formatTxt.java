f74412e15fe12387b9354485e38cd325cf2a2747
Val Villanueva <val.villanueva@norimaconsulting.com>
2022-04-29 17:03:00 +0800

    Format Text Complete

---
+++ JavaPracticeFundamentals/formatTxt.java
@@ -0,0 +1,11 @@
+public class formatTxt {
+    public static void main(String[] args) {
+
+        System.out.format("%-10s %8s %8s", "why", "not", "hello\n");
+        System.out.format("%-10s %8s %8s", "Jaguars", "10", "5\n");
+        System.out.format("%-10s %8s %8s", "Cheetahs", "14", "1\n");
+        System.out.format("%-10s %8s %8s", "Panthers", "8", "7\n");
+        System.out.format("%-10s %8s %8s", "Penguins", "4", "11\n");
+    }
+
+}
